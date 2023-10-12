769bad3f4a117615748e9dfeb3047bc44b4fcd70
phiphy28 <phi.duong@wmich.edu>
2023-10-12 13:59:04 -0400

    js changes, logic for theme change, setup for API

--- static/js/index.js
+++ static/js/index.js
@@ -1,24 +1,39 @@
-// //Variables
-// // const homeBtn = document.getElementById("home");
-// // const aboutBtn = document.getElementById("about");
-// // const catalogBtn = document.getElementById("catalog");
-// // const referencesBtn = document.getElementById("references");
-// // const technologyBtn = document.getElementById("technology");
-// // const sportsBtn = document.getElementById("sports");
-// // const financeBtn = document.getElementById("finance");
 window.addEventListener('load', function () {
+  // //Variables
+  const cards = document.querySelector('.container');
+  const homeBtn = document.getElementById('home');
+  const aboutBtn = document.getElementById('about');
+  const catalogBtn = document.getElementById('catalog');
+  const referencesBtn = document.getElementById('references');
+  const technologyBtn = document.getElementById('technology');
+  const sportsBtn = document.getElementById('sports');
+  const financeBtn = document.getElementById('finance');
 
   // Search button
   const searchBtn = document.getElementById('searchBtn');
   searchBtn.addEventListener('click', function () {
     let searchtxt = document.getElementById('searchText').value;
-    newsquery =
-      `https://newsapi.org/v2/everything?q=${searchtxt}&apiKey=19fd2331c7c643c7b91600722cbd75a2`;
+    newsquery = `https://newsapi.org/v2/everything?q=${searchtxt}&apiKey=19fd2331c7c643c7b91600722cbd75a2`;
     console.log(newsquery);
-
-    
   });
 
+  // get data
+  let pgSize = '8';
+  let pg = '1';
+  let cate = '';
+  let getData = async () => {
+    let categoryAPI =
+      `https://newsapi.org/v2/top-headlines?category=` +
+      cate +
+      `&pageSize=` +
+      pgSize +
+      `&page=` +
+      pg +
+      `&apiKey=19fd2331c7c643c7b91600722cbd75a2`;
+    let dataAPI = await fetch(categoryAPI);
+    let jsonData = await dataAPI.json();
+    console.log(jsonData);
+  };
 
   // Theme change
   let changetheme = () => {
@@ -54,6 +69,22 @@ window.addEventListener('load', function () {
   document
     .getElementById('flexSwitchCheckChecked')
     .addEventListener('change', changetheme);
+
+  // Fetching data
+  technologyBtn.addEventListener('click', function () {
+    cate = 'technology';
+    getData();
+  });
+
+  sportsBtn.addEventListener('click', function () {
+    cate = 'sports';
+    getData();
+  });
+
+  financeBtn.addEventListener('click', function () {
+    cate = 'business';
+    getData();
+  });
 });
 
 // // const newsQuery = document.getElementById('newsQuery');
@@ -73,17 +104,5 @@ window.addEventListener('load', function () {
 // });
 
 // referencesBtn.addEventListener('click', function () {
-
-// });
-
-// technologyBtn.addEventListener('click', function () {
-
-// });
-
-// sportsBtn.addEventListener('click', function () {
-
-// });
-
-// financeBtn.addEventListener('click', function () {
 
 // });
