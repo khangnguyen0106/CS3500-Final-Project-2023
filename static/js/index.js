75d85c5e2151af937da20509931cd5007c8049c8
phiphy28 <phi.duong@wmich.edu>
2023-10-12 22:39:57 -0400

    fully functional theme

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,47 @@
+// Constants for theme
+const DARK_THEME = {
+  background: 'black',
+  color: 'white',
+  navbar: ['navbar', 'bg-dark', 'navbar-dark'],
+  navbarRemove: ['navbar', 'bg-body-tertiary'],
+  label: 'Light Mode',
+};
+
+const LIGHT_THEME = {
+  background: 'white',
+  color: 'black',
+  navbar: ['navbar', 'navbar-expand-lg', 'bg-body-tertiary'],
+  navbarRemove: ['navbar', 'bg-dark', 'navbar-dark'],
+  label: 'Dark Mode',
+};
+
+function setTheme(theme) {
+  const topNav = document.getElementById('top-nav');
+  const themeLabel = document.getElementById('themeLabel');
+
+  document.body.style.backgroundColor = theme.background;
+  document.body.style.color = theme.color;
+  themeLabel.innerHTML = theme.label;
+
+  topNav.classList.remove(...theme.navbarRemove);
+  topNav.classList.add(...theme.navbar);
+}
+
+function setThemeFromLocalStorage() {
+  const theme = localStorage.getItem('theme');
+  if (theme === 'dark') {
+    setTheme(DARK_THEME);
+    document.getElementById('flexSwitchCheckChecked').checked = true;
+  } else {
+    setTheme(LIGHT_THEME);
+    document.getElementById('flexSwitchCheckChecked').checked = false;
+  }
+}
+
 window.addEventListener('load', function () {
-  // //Variables
-  const cards = document.querySelector('.container');
-  const homeBtn = document.getElementById('home');
-  const aboutBtn = document.getElementById('about');
-  const catalogBtn = document.getElementById('catalog');
-  const referencesBtn = document.getElementById('references');
+  setThemeFromLocalStorage();
+
+  // Variables
   const technologyBtn = document.getElementById('technology');
   const sportsBtn = document.getElementById('sports');
   const financeBtn = document.getElementById('finance');
@@ -12,63 +49,39 @@ window.addEventListener('load', function () {
   // Search button
   const searchBtn = document.getElementById('searchBtn');
   searchBtn.addEventListener('click', function () {
-    let searchtxt = document.getElementById('searchText').value;
-    newsquery = `https://newsapi.org/v2/everything?q=${searchtxt}&apiKey=19fd2331c7c643c7b91600722cbd75a2`;
+    const searchtxt = document.getElementById('searchText').value;
+    const newsquery = `https://newsapi.org/v2/everything?q=${searchtxt}&apiKey=19fd2331c7c643c7b91600722cbd75a2`;
     console.log(newsquery);
   });
 
-  // get data
-  let pgSize = '8';
-  let pg = '1';
+  // Get data
+  const pgSize = '8';
+  const pg = '1';
   let cate = '';
-  let getData = async () => {
-    let categoryAPI =
-      `https://newsapi.org/v2/top-headlines?category=` +
-      cate +
-      `&pageSize=` +
-      pgSize +
-      `&page=` +
-      pg +
-      `&apiKey=19fd2331c7c643c7b91600722cbd75a2`;
-    let dataAPI = await fetch(categoryAPI);
-    let jsonData = await dataAPI.json();
-    console.log(jsonData);
-  };
+
+  async function getData() {
+    try {
+      const categoryAPI = `https://newsapi.org/v2/top-headlines?category=${cate}&pageSize=${pgSize}&page=${pg}&apiKey=19fd2331c7c643c7b91600722cbd75a2`;
+      const dataAPI = await fetch(categoryAPI);
+      const jsonData = await dataAPI.json();
+      console.log(jsonData);
+    } catch (error) {
+      console.error('Error fetching the news data:', error);
+    }
+  }
 
   // Theme change
-  let changetheme = () => {
-    // try {
-    //   theme_ = localStorage.getItem("theme")
-    // }
-    // catch {
-
-    let themeLabel = this.document.getElementById('themeLabel');
-    if (document.getElementById('flexSwitchCheckChecked').checked) {
-      this.localStorage.setItem('theme', 'dark');
-      document.body.style.backgroundColor = 'black';
-      document.body.style.color = 'white';
-      themeLabel.innerHTML = 'Light Mode';
-      topNav = document.getElementById('top-nav');
-
-      // Navbar
-      topNav.classList.remove('navbar', 'bg-body-tertiary');
-      topNav.classList.add('navbar', 'bg-dark', 'navbar-dark');
-    } else {
-      this.localStorage.setItem('theme', 'light');
-      document.body.style.backgroundColor = 'white';
-      document.body.style.color = 'black';
-      themeLabel.innerHTML = 'Dark Mode';
-
-      // Navbar
-      topNav.classList.remove('navbar', 'bg-dark', 'navbar-dark');
-      topNav.classList.add('navbar', 'navbar-expand-lg', 'bg-body-tertiary');
-    }
-    // }
-  };
-
   document
     .getElementById('flexSwitchCheckChecked')
-    .addEventListener('change', changetheme);
+    .addEventListener('change', function () {
+      if (this.checked) {
+        localStorage.setItem('theme', 'dark');
+        setTheme(DARK_THEME);
+      } else {
+        localStorage.setItem('theme', 'light');
+        setTheme(LIGHT_THEME);
+      }
+    });
 
   // Fetching data
   technologyBtn.addEventListener('click', function () {
@@ -86,6 +99,7 @@ window.addEventListener('load', function () {
     getData();
   });
 });
+
 
 // // const newsQuery = document.getElementById('newsQuery');
 // //need to add stuff here (min 17)
