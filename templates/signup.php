4c4b97fc8e5b65696fb18933e6ee4d1a2739050c
khangnguyen0106 <140562323+khangnguyen0106@users.noreply.github.com>
2023-12-08 21:06:51 -0500

    Update signup.php 2

--- templates/signup.php
+++ templates/signup.php
@@ -66,12 +66,12 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
   integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
    crossorigin="anonymous" />
- <link rel="stylesheet"  href="main.css"/>
+ <link rel="stylesheet"  href="../static/cssmain.css"/>
   <!-- Link to Bootstrap's JS and then main.js -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
     integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
     crossorigin="anonymous"></script>
-  <script src="index.js"></script>
+  <script src="../static/js/index.js"></script>
     <style>
         body {
             background-color: #f8f9fa;
